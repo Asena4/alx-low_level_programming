724b4d4383be3840af0e8d4a094f8f34cf161c01
Asena4 <asenakevin@gmail.com>
2023-10-16 13:00:36 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line
+ * @s: string
+ * return: 0
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+	int o;
+
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	s--;
+	for (o = i; o > 0; o--)
+	{
+		_putchar(*s);
+		s--;
+	}
+
+	_putchar('\n');
+}
