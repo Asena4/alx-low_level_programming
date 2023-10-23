646c490cca1eecf0d447f6db00b920c238737b08
Asena4 <asenakevin@gmail.com>
2023-10-23 12:27:50 -0700

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * _strstr - the main function
+  * @needle: function parameter
+  * @haystack: function parameter
+  * Return: value is 0
+  */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+	char *h = haystack;
+	char *n = needle;
+
+	while (*h == *n && *n != '\0')
+	{
+	h++;
+	n++;
+	}
+	if (*n == '\0')
+	return (haystack);
+	}
+	return (0);
+}
