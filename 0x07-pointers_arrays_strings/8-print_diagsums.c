3a647c732c98a7a6f8a4c61cbf6d6f91f2900967
Asena4 <asenakevin@gmail.com>
2023-10-23 12:40:10 -0700

    8-print_diagsums.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * print_diagsums - the main function
+  * @size: function parameter
+  * @a: function parameter
+  * Return: value is 0
+  */
+void print_diagsums(int *a, int size)
+{
+	int count1;
+	int count2;
+	int i;
+
+	ct1 = 0;
+	ct2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		ct1 = ct1 + a[i * size + i];
+	}
+	for (i = size - 1; i >= 0; i--)
+	{
+		ct2 += a[i * size + (size - i - 1)];
+	}
+	printf("%d, %d\n", ct1, ct2);
+}
