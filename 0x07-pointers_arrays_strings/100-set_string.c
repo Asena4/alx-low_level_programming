d48df982b51c5ccd88bfa02ff95124320b73a6d7
Asena4 <asenakevin@gmail.com>
2023-10-23 12:49:28 -0700

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+  * set_string - the main function
+  * @s: function parameter
+  * @to: function parameter
+  * Return: value is 0
+  */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
