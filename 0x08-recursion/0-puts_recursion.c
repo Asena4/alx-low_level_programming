2a9bcdbd46285d3f53f1661f31b5ed0166a53838
Asena4 <asenakevin@gmail.com>
2023-10-25 12:34:18 -0700

    Prints a string, followed by a new line

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * _puts_recursion - prints a string, followed by a new line
+  * @s: function parameter
+  */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+This directory contains recursion projects
