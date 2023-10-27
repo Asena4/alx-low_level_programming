4b1c47baba567331b4a6889c4a3ab1134a5e041a
Asena4 <asenakevin@gmail.com>
2023-10-27 14:43:01 -0700

    Prints its name, followed by a new line

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the name of the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: value is 0
+ */
+int main(int argc, char *argv[])
+{
+	void (argc);
+
+	printf("%s\n", *argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This directory contains projects for argc and argv
