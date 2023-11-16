e94ac5537e7d699fb2261e3bd0c19c785107670d
Asena4 <asenakevin@gmail.com>
2023-11-16 14:28:34 -0800

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,7 +11,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *newnode;
 	list_t *temp = *head;
 	unsigned int len = 0;
-	
+
 	while (str[len])
 		len++;
 
@@ -28,7 +28,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		*head = newnode;
 		return (newnode);
 	}
-	
+
 	while (temp->next)
 		temp = temp->next;
 	temp->next = newnode;
