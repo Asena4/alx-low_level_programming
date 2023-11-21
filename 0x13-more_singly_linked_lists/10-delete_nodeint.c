3916978eef5b991596d8f90e9d320541676fe75d
Asena4 <asenakevin@gmail.com>
2023-11-21 14:06:14 -0800

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -28,7 +28,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		i++;
 	}
 	current = tempnode->next;
-	temp->next = current->next;
+	tempnode->next = current->next;
 	free(current);
 	return (1);
 }
